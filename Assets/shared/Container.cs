f627850d4fb5f7d03c9b0c8cfa08cca9fb6f48cd
Tyler Jones <tylerjones81@gmail.com>
2018-08-02 15:16:14 -0400

    scene

---
+++ Assets/shared/Container.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Container : MonoBehaviour {
+
+    private class ContainerItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Maximum { get; set; }
+
+        private int amountTaken;
+
+    }
+
+    List<ContainerItem> items;
+
+    private void Awake()
+    {
+        items = new List<ContainerItem>();
+    }
+
+    public Guid Add(string name, int maximum)
+    {
+        items.Add(new ContainerItem {
+            Id = System.Guid.NewGuid(),
+            Maximum = maximum,
+            Name =name
+        });
+
+        return items.Last().Id;
+    }
+}
